12587674383da8c36b317e00fc486b6bc516c76c
gfh16 <gfh16@mails.tsinghua.edu.cn>
2020-05-27 12:34:11 +0800

    modified PIDDrawL1L2.C

--- Step0_PID_Draw/PIDDrawL1L2.C
+++ Step0_PID_Draw/PIDDrawL1L2.C
@@ -1,6 +1,6 @@
 //_________________________
 // definition of constants
-Int_t FirstRun = 200;
+Int_t FirstRun = 350;
 Int_t LastRun  = 400;
 Int_t SSDNum   = 4;
 Int_t CHNum    = 16;
@@ -12,7 +12,7 @@ Int_t NBinsY   = 4096;
 Int_t HitMultiCut = 1;
 
 Int_t L1S_CHIndex = 4;
-Int_t L2F_CHIndex = 12;
+Int_t L2F_CHIndex = 13;
 Int_t L2B_CHIndex = 4;
 Int_t L3A_CHIndex = 6;
 
@@ -25,7 +25,7 @@ void DeleteData(Double_t*** p, Int_t& ssdnum, Int_t& chnum, Int_t& parnum);
 void AddChain(TChain *chain, const char *LayerTag, Int_t *dataarray, Int_t size, Int_t index);
 
 //____________________________
-void PIDDraw()
+void PIDDrawL1L2()
 {
   gStyle->SetPalette(1);
   gStyle->SetOptStat(0);
@@ -56,26 +56,8 @@ void PIDDraw()
   std::string L2BTag("L2B");
   std::string L3ATag("L3A");
 
-  ofstream FileOut_L2F_L3[SSDNum];
-  ofstream FileOut_L2B_L3[SSDNum];
-  std::string pathOut_L2F_L3[SSDNum];
-  std::string pathOut_L2B_L3[SSDNum];
-  for (Int_t i=0; i<SSDNum; i++)
-  {
-    EnrtyNum_L2F_L3[i] = 0;
-    EnrtyNum_L2B_L3[i] = 0;
-
-    pathOut_L2F_L3[i] = Form("output/SSD%d_L2FCH%02d_L3CH%02d.txt",i+1,L2F_CHIndex,L3A_CHIndex);
-    pathOut_L2B_L3[i] = Form("output/SSD%d_L2BCH%02d_L3CH%02d.txt",i+1,L2B_CHIndex,L3A_CHIndex);
-    FileOut_L2F_L3[i].open(pathOut_L2F_L3[i].c_str());
-    FileOut_L2B_L3[i].open(pathOut_L2B_L3[i].c_str());
-
-    FileOut_L2F_L3[i]<<"* EntryNumber"<<setw(25)<<"E_{CsI} (ADC Channel)"<<setw(25)<<"dE_{F} (ADC Channel))\n";
-    FileOut_L2B_L3[i]<<"* EntryNumber"<<setw(25)<<"E_{CsI} (ADC Channel)"<<setw(25)<<"dE_{B} (ADC Channel))\n";
-  }
-
-  std::string pathOut_Cans_PID_L2F_L3(Form("output/SSD_L2FCH%02d_L3CH%02d.png",L2F_CHIndex,L3A_CHIndex));
-  std::string pathOut_Cans_PID_L2B_L3(Form("output/SSD_L2BCH%02d_L3CH%02d.png",L2B_CHIndex,L3A_CHIndex));
+  std::string pathOut_Cans_PID_L1_L2F(Form("output/SSD_L1CH%02d_L2FCH%02d.png",L1S_CHIndex,L2F_CHIndex));
+  std::string pathOut_Cans_PID_L1_L2B(Form("output/SSD_L1CH%02d_L2BCH%02d.png",L1S_CHIndex,L2B_CHIndex));
 
   std::string pathFolder("/home/sea/Fission2019-Unified-Analysis-Framework"
                          "/Step4_SSD-Analysis-Framework/Step4.1_EnergyCalibration/output/");
@@ -170,11 +152,6 @@ void PIDDraw()
         if ((SSD_L2F_E[ssdnum][L2F_CHIndex]>SSD_L2FCut[ssdnum][L2F_CHIndex]) &&
         (SSD_L3A_E[ssdnum][L3A_CHIndex]>L3NoiseCut)) {
           PID_L2F_L3[ssdnum]->Fill(SSD_L3A_E[ssdnum][L3A_CHIndex],SSD_L2F_E[ssdnum][L2F_CHIndex]);
-
-          EnrtyNum_L2F_L3[ssdnum]++;
-          FileOut_L2F_L3[ssdnum]<<setw(5)<<std::right<<EnrtyNum_L2F_L3[ssdnum]
-                                <<setw(20)<<std::right<<SSD_L3A_E[ssdnum][L3A_CHIndex]
-                                <<setw(20)<<std::right<<SSD_L2F_E[ssdnum][L2F_CHIndex] <<endl;
         }
       }
       // PID_L2B_L3
@@ -182,30 +159,25 @@ void PIDDraw()
         if ((SSD_L2B_E[ssdnum][L2B_CHIndex]>SSD_L2BCut[ssdnum][L2B_CHIndex]) &&
         (SSD_L3A_E[ssdnum][L3A_CHIndex]>L3NoiseCut)) {
           PID_L2B_L3[ssdnum]->Fill(SSD_L3A_E[ssdnum][L3A_CHIndex],SSD_L2B_E[ssdnum][L2B_CHIndex]);
-
-          EnrtyNum_L2B_L3[ssdnum]++;
-          FileOut_L2B_L3[ssdnum]<<setw(5)<<std::right<<EnrtyNum_L2B_L3[ssdnum]
-                                <<setw(20)<<std::right<<SSD_L3A_E[ssdnum][L3A_CHIndex]
-                                <<setw(20)<<std::right<<SSD_L2B_E[ssdnum][L2B_CHIndex]<<endl;
         }
       }
     }
   }  // end the loop of entries
 
   //______________________________________
-  TCanvas *Cans_PID_L2F_L3 = new TCanvas("PID_L2F_L3","PID_L2F_L3",1000,1000);
-  TCanvas *Cans_PID_L2B_L3 = new TCanvas("PID_L2B_L3","PID_L2B_L3",1000,1000);
-  Cans_PID_L2F_L3->Divide(2,2,small,small);
-  Cans_PID_L2B_L3->Divide(2,2,small,small);
+  TCanvas *Cans_PID_L1_L2F = new TCanvas("PID_L1_L2F","PID_L1_L2F",1000,1000);
+  TCanvas *Cans_PID_L1_L2B = new TCanvas("PID_L1_L2B","PID_L1_L2B",1000,1000);
+  Cans_PID_L1_L2F->Divide(2,2,small,small);
+  Cans_PID_L1_L2B->Divide(2,2,small,small);
 
-  TLatex *ssd1 = new TLatex(1700.,3700.,Form("SSD1_L2FCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd2 = new TLatex(1700.,3700.,Form("SSD2_L2FCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd3 = new TLatex(1700.,3700.,Form("SSD3_L2FCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd4 = new TLatex(1700.,3700.,Form("SSD4_L2FCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd11 = new TLatex(1700.,3700.,Form("SSD1_L2BCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd22 = new TLatex(1700.,3700.,Form("SSD2_L2BCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd33 = new TLatex(1700.,3700.,Form("SSD3_L2BCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
-  TLatex *ssd44 = new TLatex(1700.,3700.,Form("SSD4_L2BCH%02d_L3CH%02d",L2F_CHIndex,L3A_CHIndex));
+  TLatex *ssd1  = new TLatex(1700.,3700.,Form("SSD1_L1CH%02d_L2FCH%02d",L1S_CHIndex,L2F_CHIndex));
+  TLatex *ssd2  = new TLatex(1700.,3700.,Form("SSD2_L1CH%02d_L2FCH%02d",L1S_CHIndex,L2F_CHIndex));
+  TLatex *ssd3  = new TLatex(1700.,3700.,Form("SSD3_L1CH%02d_L2FCH%02d",L1S_CHIndex,L2F_CHIndex));
+  TLatex *ssd4  = new TLatex(1700.,3700.,Form("SSD4_L1CH%02d_L2FCH%02d",L1S_CHIndex,L2F_CHIndex));
+  TLatex *ssd11 = new TLatex(1700.,3700.,Form("SSD1_L1CH%02d_L2BCH%02d",L1S_CHIndex,L2B_CHIndex));
+  TLatex *ssd22 = new TLatex(1700.,3700.,Form("SSD2_L1CH%02d_L2BCH%02d",L1S_CHIndex,L2B_CHIndex));
+  TLatex *ssd33 = new TLatex(1700.,3700.,Form("SSD3_L1CH%02d_L2BCH%02d",L1S_CHIndex,L2B_CHIndex));
+  TLatex *ssd44 = new TLatex(1700.,3700.,Form("SSD4_L1CH%02d_L2BCH%02d",L1S_CHIndex,L2B_CHIndex));
   ssd1->SetTextColor(kRed);
   ssd2->SetTextColor(kRed);
   ssd3->SetTextColor(kRed);
@@ -223,165 +195,165 @@ void PIDDraw()
   ssd33->SetTextSize(0.04);
   ssd44->SetTextSize(0.04);
 
-  Cans_PID_L2F_L3->cd(1);
+  Cans_PID_L1_L2F->cd(1);
   gPad->SetBottomMargin(small);
   gPad->SetRightMargin(small);
   gPad->SetLeftMargin(0.18);
   gPad->SetTopMargin(0.18);
-  PID_L2F_L3[0]->SetTitle("");
-  PID_L2F_L3[0]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2F_L3[0]->GetXaxis()->SetNdivisions(1010);
-  PID_L2F_L3[0]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[0]->GetYaxis()->SetRangeUser(1.,4000.);
-  PID_L2F_L3[0]->GetYaxis()->SetNdivisions(1008);
-  PID_L2F_L3[0]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[0]->GetYaxis()->SetTitle("dE_{F} (ADC CH)");
-  PID_L2F_L3[0]->GetYaxis()->SetTitleSize(0.05);
-  PID_L2F_L3[0]->GetYaxis()->CenterTitle(1);
-  PID_L2F_L3[0]->GetYaxis()->SetTitleOffset(1.7);
-  PID_L2F_L3[0]->Draw("COL");
+  PID_L1_L2F[0]->SetTitle("");
+  PID_L1_L2F[0]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2F[0]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2F[0]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[0]->GetYaxis()->SetRangeUser(1.,4000.);
+  PID_L1_L2F[0]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2F[0]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[0]->GetYaxis()->SetTitle("dE_{F} (ADC CH)");
+  PID_L1_L2F[0]->GetYaxis()->SetTitleSize(0.05);
+  PID_L1_L2F[0]->GetYaxis()->CenterTitle(1);
+  PID_L1_L2F[0]->GetYaxis()->SetTitleOffset(1.7);
+  PID_L1_L2F[0]->Draw("COL");
   ssd1->Draw("SAME");
 
-  Cans_PID_L2F_L3->cd(2);
+  Cans_PID_L1_L2F->cd(2);
   gPad->SetBottomMargin(small);
   gPad->SetLeftMargin(small);
   gPad->SetRightMargin(0.18);
   gPad->SetTopMargin(0.18);
-  PID_L2F_L3[1]->SetTitle("");
-  PID_L2F_L3[1]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2F_L3[1]->GetXaxis()->SetNdivisions(1010);
-  PID_L2F_L3[1]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[1]->GetYaxis()->SetRangeUser(1.,4000.);
-  PID_L2F_L3[1]->GetYaxis()->SetNdivisions(1008);
-  PID_L2F_L3[1]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[1]->Draw("COL");
+  PID_L1_L2F[1]->SetTitle("");
+  PID_L1_L2F[1]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2F[1]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2F[1]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[1]->GetYaxis()->SetRangeUser(1.,4000.);
+  PID_L1_L2F[1]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2F[1]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[1]->Draw("COL");
   ssd2->Draw("SAME");
 
-  Cans_PID_L2F_L3->cd(3);
+  Cans_PID_L1_L2F->cd(3);
   gPad->SetTopMargin(small);
   gPad->SetRightMargin(small);
   gPad->SetLeftMargin(0.18);
   gPad->SetBottomMargin(0.18);
   gPad->SetTickx();
-  PID_L2F_L3[2]->SetTitle("");
-  PID_L2F_L3[2]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2F_L3[2]->GetXaxis()->SetNdivisions(1010);
-  PID_L2F_L3[2]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[2]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
-  PID_L2F_L3[2]->GetXaxis()->CenterTitle(1);
-  PID_L2F_L3[2]->GetXaxis()->SetTitleSize(0.05);
-  PID_L2F_L3[2]->GetXaxis()->SetTitleOffset(1.1);
-  PID_L2F_L3[2]->GetYaxis()->SetRangeUser(1.,3999.);
-  PID_L2F_L3[2]->GetYaxis()->SetNdivisions(1008);
-  PID_L2F_L3[2]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[2]->GetYaxis()->SetTitle("dE_{F} (ADC CH)");
-  PID_L2F_L3[2]->GetYaxis()->CenterTitle(1);
-  PID_L2F_L3[2]->GetYaxis()->SetTitleOffset(1.7);
-  PID_L2F_L3[2]->GetYaxis()->SetTitleSize(0.05);
-  PID_L2F_L3[2]->Draw("COL");
+  PID_L1_L2F[2]->SetTitle("");
+  PID_L1_L2F[2]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2F[2]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2F[2]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[2]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
+  PID_L1_L2F[2]->GetXaxis()->CenterTitle(1);
+  PID_L1_L2F[2]->GetXaxis()->SetTitleSize(0.05);
+  PID_L1_L2F[2]->GetXaxis()->SetTitleOffset(1.1);
+  PID_L1_L2F[2]->GetYaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2F[2]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2F[2]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[2]->GetYaxis()->SetTitle("dE_{F} (ADC CH)");
+  PID_L1_L2F[2]->GetYaxis()->CenterTitle(1);
+  PID_L1_L2F[2]->GetYaxis()->SetTitleOffset(1.7);
+  PID_L1_L2F[2]->GetYaxis()->SetTitleSize(0.05);
+  PID_L1_L2F[2]->Draw("COL");
   ssd3->Draw("SAME");
 
-  Cans_PID_L2F_L3->cd(4);
+  Cans_PID_L1_L2F->cd(4);
   gPad->SetTopMargin(small);
   gPad->SetLeftMargin(small);
   gPad->SetRightMargin(0.18);
   gPad->SetBottomMargin(0.18);
   gPad->SetTickx();
-  PID_L2F_L3[3]->SetTitle("");
-  PID_L2F_L3[3]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2F_L3[3]->GetXaxis()->SetNdivisions(1010);
-  PID_L2F_L3[3]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[3]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
-  PID_L2F_L3[3]->GetXaxis()->SetTitleSize(0.05);
-  PID_L2F_L3[3]->GetXaxis()->CenterTitle(1);
-  PID_L2F_L3[3]->GetXaxis()->SetTitleOffset(1.1);
-  PID_L2F_L3[3]->GetYaxis()->SetRangeUser(0.,4000.);
-  PID_L2F_L3[3]->GetYaxis()->SetNdivisions(1008);
-  PID_L2F_L3[3]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2F_L3[3]->Draw("COL");
+  PID_L1_L2F[3]->SetTitle("");
+  PID_L1_L2F[3]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2F[3]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2F[3]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[3]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
+  PID_L1_L2F[3]->GetXaxis()->SetTitleSize(0.05);
+  PID_L1_L2F[3]->GetXaxis()->CenterTitle(1);
+  PID_L1_L2F[3]->GetXaxis()->SetTitleOffset(1.1);
+  PID_L1_L2F[3]->GetYaxis()->SetRangeUser(0.,4000.);
+  PID_L1_L2F[3]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2F[3]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2F[3]->Draw("COL");
   ssd4->Draw("SAME");
 
 
-  Cans_PID_L2B_L3->cd(1);
+  Cans_PID_L1_L2B->cd(1);
   gPad->SetBottomMargin(small);
   gPad->SetRightMargin(small);
   gPad->SetLeftMargin(0.18);
   gPad->SetTopMargin(0.18);
-  PID_L2B_L3[0]->SetTitle("");
-  PID_L2B_L3[0]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2B_L3[0]->GetXaxis()->SetNdivisions(1010);
-  PID_L2B_L3[0]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[0]->GetYaxis()->SetRangeUser(1.,4000.);
-  PID_L2B_L3[0]->GetYaxis()->SetNdivisions(1008);
-  PID_L2B_L3[0]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[0]->GetYaxis()->SetTitle("dE_{B} (ADC CH)");
-  PID_L2B_L3[0]->GetYaxis()->SetTitleSize(0.05);
-  PID_L2B_L3[0]->GetYaxis()->CenterTitle(1);
-  PID_L2B_L3[0]->GetYaxis()->SetTitleOffset(1.7);
-  PID_L2B_L3[0]->Draw("COL");
+  PID_L1_L2B[0]->SetTitle("");
+  PID_L1_L2B[0]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2B[0]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2B[0]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[0]->GetYaxis()->SetRangeUser(1.,4000.);
+  PID_L1_L2B[0]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2B[0]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[0]->GetYaxis()->SetTitle("dE_{L1} (ADC CH)");
+  PID_L1_L2B[0]->GetYaxis()->SetTitleSize(0.05);
+  PID_L1_L2B[0]->GetYaxis()->CenterTitle(1);
+  PID_L1_L2B[0]->GetYaxis()->SetTitleOffset(1.7);
+  PID_L1_L2B[0]->Draw("COL");
   ssd11->Draw("SAME");
 
-  Cans_PID_L2B_L3->cd(2);
+  Cans_PID_L1_L2B->cd(2);
   gPad->SetBottomMargin(small);
   gPad->SetLeftMargin(small);
   gPad->SetRightMargin(0.18);
   gPad->SetTopMargin(0.18);
-  PID_L2B_L3[1]->SetTitle("");
-  PID_L2B_L3[1]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2B_L3[1]->GetXaxis()->SetNdivisions(1010);
-  PID_L2B_L3[1]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[1]->GetYaxis()->SetRangeUser(1.,4000.);
-  PID_L2B_L3[1]->GetYaxis()->SetNdivisions(1008);
-  PID_L2B_L3[1]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[1]->Draw("COL");
+  PID_L1_L2B[1]->SetTitle("");
+  PID_L1_L2B[1]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2B[1]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2B[1]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[1]->GetYaxis()->SetRangeUser(1.,4000.);
+  PID_L1_L2B[1]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2B[1]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[1]->Draw("COL");
   ssd22->Draw("SAME");
 
-  Cans_PID_L2B_L3->cd(3);
+  Cans_PID_L1_L2B->cd(3);
   gPad->SetTopMargin(small);
   gPad->SetRightMargin(small);
   gPad->SetLeftMargin(0.18);
   gPad->SetBottomMargin(0.18);
   gPad->SetTickx();
-  PID_L2B_L3[2]->SetTitle("");
-  PID_L2B_L3[2]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2B_L3[2]->GetXaxis()->SetNdivisions(1010);
-  PID_L2B_L3[2]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[2]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
-  PID_L2B_L3[2]->GetXaxis()->CenterTitle(1);
-  PID_L2B_L3[2]->GetXaxis()->SetTitleSize(0.05);
-  PID_L2B_L3[2]->GetXaxis()->SetTitleOffset(1.1);
-  PID_L2B_L3[2]->GetYaxis()->SetRangeUser(1.,3999.);
-  PID_L2B_L3[2]->GetYaxis()->SetNdivisions(1008);
-  PID_L2B_L3[2]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[2]->GetYaxis()->SetTitle("dE_{B} (ADC CH)");
-  PID_L2B_L3[2]->GetYaxis()->CenterTitle(1);
-  PID_L2B_L3[2]->GetYaxis()->SetTitleOffset(1.7);
-  PID_L2B_L3[2]->GetYaxis()->SetTitleSize(0.05);
-  PID_L2B_L3[2]->Draw("COL");
+  PID_L1_L2B[2]->SetTitle("");
+  PID_L1_L2B[2]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2B[2]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2B[2]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[2]->GetXaxis()->SetTitle("E_{L2B} (ADC CH)");
+  PID_L1_L2B[2]->GetXaxis()->CenterTitle(1);
+  PID_L1_L2B[2]->GetXaxis()->SetTitleSize(0.05);
+  PID_L1_L2B[2]->GetXaxis()->SetTitleOffset(1.1);
+  PID_L1_L2B[2]->GetYaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2B[2]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2B[2]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[2]->GetYaxis()->SetTitle("dE_{L1} (ADC CH)");
+  PID_L1_L2B[2]->GetYaxis()->CenterTitle(1);
+  PID_L1_L2B[2]->GetYaxis()->SetTitleOffset(1.7);
+  PID_L1_L2B[2]->GetYaxis()->SetTitleSize(0.05);
+  PID_L1_L2B[2]->Draw("COL");
   ssd33->Draw("SAME");
 
-  Cans_PID_L2B_L3->cd(4);
+  Cans_PID_L1_L2B->cd(4);
   gPad->SetTopMargin(small);
   gPad->SetLeftMargin(small);
   gPad->SetRightMargin(0.18);
   gPad->SetBottomMargin(0.18);
   gPad->SetTickx();
-  PID_L2B_L3[3]->SetTitle("");
-  PID_L2B_L3[3]->GetXaxis()->SetRangeUser(1.,3999.);
-  PID_L2B_L3[3]->GetXaxis()->SetNdivisions(1010);
-  PID_L2B_L3[3]->GetXaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[3]->GetXaxis()->SetTitle("E_{CsI} (ADC CH)");
-  PID_L2B_L3[3]->GetXaxis()->SetTitleSize(0.05);
-  PID_L2B_L3[3]->GetXaxis()->CenterTitle(1);
-  PID_L2B_L3[3]->GetXaxis()->SetTitleOffset(1.1);
-  PID_L2B_L3[3]->GetYaxis()->SetRangeUser(0.,4000.);
-  PID_L2B_L3[3]->GetYaxis()->SetNdivisions(1008);
-  PID_L2B_L3[3]->GetYaxis()->SetLabelSize(0.05);
-  PID_L2B_L3[3]->Draw("COL");
+  PID_L1_L2B[3]->SetTitle("");
+  PID_L1_L2B[3]->GetXaxis()->SetRangeUser(1.,3999.);
+  PID_L1_L2B[3]->GetXaxis()->SetNdivisions(1010);
+  PID_L1_L2B[3]->GetXaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[3]->GetXaxis()->SetTitle("E_{L2B} (ADC CH)");
+  PID_L1_L2B[3]->GetXaxis()->SetTitleSize(0.05);
+  PID_L1_L2B[3]->GetXaxis()->CenterTitle(1);
+  PID_L1_L2B[3]->GetXaxis()->SetTitleOffset(1.1);
+  PID_L1_L2B[3]->GetYaxis()->SetRangeUser(0.,4000.);
+  PID_L1_L2B[3]->GetYaxis()->SetNdivisions(1008);
+  PID_L1_L2B[3]->GetYaxis()->SetLabelSize(0.05);
+  PID_L1_L2B[3]->Draw("COL");
   ssd44->Draw("SAME");
 
-  Cans_PID_L2F_L3->Print(pathOut_Cans_PID_L2F_L3.c_str());
-  Cans_PID_L2B_L3->Print(pathOut_Cans_PID_L2B_L3.c_str());
+  Cans_PID_L1_L2F->Print(pathOut_Cans_PID_L1_L2F.c_str());
+  Cans_PID_L1_L2B->Print(pathOut_Cans_PID_L1_L2B.c_str());
 
 }
 
